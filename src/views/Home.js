26c604deadc24ae82bc3d80d364cb714300e623a
Branden Lee <bdleecs95@gmail.com>
2020-09-26 15:43:32 -0400

    Adding Data Tiles

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,12 +5,14 @@ import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
 import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
+import DataTiles from "../components/sections/DataTiles";
 
 const Home = () => {
 
   return (
     <>
       <Hero className="illustration-section-01" />
+      <DataTiles />
       <FeaturesTiles />
       <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
       <Testimonial topDivider />
